117806c44130b4447f8ea60a928188e16029d016
Bert Frees <bertfrees@gmail.com>
2011-11-06 16:23:08 +0100

    added license information

--- addon/be/docarch/accessodf/ooo/CentralRegistrationClass.java
+++ addon/be/docarch/accessodf/ooo/CentralRegistrationClass.java
@@ -1,10 +1,23 @@
-/*
- * CentralRegistrationClass.java
+/**
+ *  AccessODF - Accessibility checker for OpenOffice.org and LibreOffice Writer.
  *
- * Created on 2010.11.16 - 15:35:25
+ *  Copyright (c) 2011 by DocArch <http://www.docarch.be>.
  *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package be.docarch.accessodf.ooo;
+ 
+ package be.docarch.accessodf.ooo;
 
 import com.sun.star.lang.XSingleComponentFactory;
 import com.sun.star.registry.XRegistryKey;
