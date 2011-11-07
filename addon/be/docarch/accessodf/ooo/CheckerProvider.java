f3cb88d35f8037a879db9db2d893859c836d0601
Bert Frees <bert.frees@esat.kuleuven.be>
2011-11-07 17:54:00 +0100

    OpenOffice.org-minimal-version

--- addon/be/docarch/accessodf/ooo/CheckerProvider.java
+++ addon/be/docarch/accessodf/ooo/CheckerProvider.java
@@ -16,8 +16,8 @@
  *  You should have received a copy of the GNU Lesser General Public License
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
- 
- package be.docarch.accessodf.ooo;
+
+package be.docarch.accessodf.ooo;
 
 import be.docarch.accessodf.Constants;
 import be.docarch.accessodf.Checker;
--- addon/be/docarch/accessodf/ooo/Table.java
+++ addon/be/docarch/accessodf/ooo/Table.java
@@ -16,8 +16,8 @@
  *  You should have received a copy of the GNU Lesser General Public License
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
- 
- package be.docarch.accessodf.ooo;
+
+package be.docarch.accessodf.ooo;
 
 import com.sun.star.uno.UnoRuntime;
 import com.sun.star.container.XNamed;
